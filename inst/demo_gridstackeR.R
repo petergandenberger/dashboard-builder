e1672f56667b4c6a6cba1baf9eb32bff15fd7ef0
Peter Gandenberger <peter.gandenberger@gmail.com>
2021-08-05 18:49:40 +0200

    added resize functionality

--- inst/demo_gridstackeR.R
+++ inst/demo_gridstackeR.R
@@ -7,15 +7,16 @@ ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(),
   dashboardBody(
-    tags$style(".col-sm-12{height:100%;}"),
+    # make sure the content fills the given height
+    tags$style(".grid-stack-item-content .col-sm-12{height:100%;}"),
     #grid layout
     grid_stack(
       grid_stack_item(
-        h = 2, w = 2,
+        h = 2, w = 2, noResize = T,
         sliderInput(inputId = "bins", label = "Number of bins:", min = 1, max = 50,value = 30)
       ),
       grid_stack_item(
-        h = 6, w = 4,
+        id = "distPlotContainer", h = 6, w = 4,
         plotOutput(outputId = "distPlot")
       )
     )
@@ -31,8 +32,7 @@ server <- function(input, output) {
     hist(x, breaks = bins, col = "#75AADB", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
-
-  })
+  }, height = function() {max(input$distPlotContainer_height, 420) - 50})
 
 }
 
